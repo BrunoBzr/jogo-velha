8472764258c354beea1012292a2df708845e5b31
Bruno Pereira <brunopereirabzrdev@gmail.com>
2024-11-02 21:25:32 -0300

    criando a função nomeJogador
    Função nomeJogador irar ficar responsável pela captura no nome dos jogadores e letra escolhida do jogador 1.

--- jogoVelha.c
+++ jogoVelha.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
 	char nome[50];
 	char letra;
-};
+}Jogador;
 
-
+void NomeJogadores(Jogador *jogador);
+void Letra(Jogador *jogador);
 void main(){
-	
+	Jogador jogadores[2];
 	
 	while(1){
 		int op;
@@ -25,6 +27,9 @@ void main(){
 		switch(op)
 		{
 		case 1:
+			fflush(stdin);
+			system("cls");
+			NomeJogadores(jogadores);
 			break;
 		default:
 			printf("Opcao invalida!");
@@ -33,6 +38,35 @@ void main(){
 	}//fim while
 }
 
-void NomeJogadores(){
+void NomeJogadores(Jogador *jogador){
+	printf("---------- JOGO DA VELHA ----------\n");
+	printf("Informe o nome do jogador 1: ");
+	gets(jogador[0].nome);
+	printf("Informe o nome do jogador 2: ");
+	gets(jogador[1].nome);
+	Letra(jogador);
+	printf("Nome joagdor 1: %s\n", jogador[0].nome);
+	printf("Nome joagdor 2: %s\n", jogador[1].nome);
+	printf("Letra jogador 1: %c\n", jogador[0].letra);
+	printf("Letra jogador 2: %c\n", jogador[1].letra);
+	system("pause");
+}
+
+void Letra(Jogador *jogador){
+	while(jogador[0].letra != 'X' || jogador[0].letra != 'O'){
+		system("cls");
+		printf("%s voce vai querer X ou O? ", jogador[0].nome);
+		scanf("%c", toupper(&jogador[0].letra));
+		
+		printf("Letra jogador 1: %c\n", jogador[0].letra);
+		system("pause");
+		
+		if(jogador[0].letra != 'X' || jogador[0].letra != 'O'){
+			printf("Letra invalida!\n");
+		}
+		if(jogador[0].letra == 'X'){
+			jogador[1].letra = 'O';
+		}
+	}//fim while
 	
 }
